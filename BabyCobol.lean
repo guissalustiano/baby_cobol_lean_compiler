bc0fa515310e7c4aa443dd2d5d1e6c4bf7209304
Guilherme S. Salustiano <guissalustiano@gmail.com>
2024-02-28 17:06:57 +0100

    scaffold haskell

--- BabyCobol.lean
+++
@@ -1,3 +0,0 @@
--- This module serves as the root of the `BabyCobol` library.
--- Import modules here that should be built as part of the library.
-import «BabyCobol».Basic
--- Main.lean
+++
@@ -1,4 +0,0 @@
-import «BabyCobol»
-
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
