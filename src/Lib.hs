a2cb466416ffef79d984cc7bebb2d105e6d63768
Guilherme S. Salustiano <guissalustiano@gmail.com>
2024-02-29 21:29:39 +0100

    add identifier parser

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+
 module Lib
-    ( someFunc
+    ( 
+    Parser,
+    Identifier(..),
+    identifier
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Void
+import Text.Megaparsec hiding (State)
+import Text.Megaparsec.Char
+import Control.Applicative hiding (many)
+import Control.Monad
+import qualified Data.Text as T
+import qualified Text.Megaparsec.Char.Lexer as L
+
+type Parser = Parsec Void String
+
+newtype Identifier = Identifier String
+    deriving (Show, Eq)
+
+-- From https://serokell.io/blog/parser-combinators-in-haskell#identifiers
+identifier :: Parser Identifier
+identifier = Identifier <$> label "identifier" ((:) <$> (letterChar <|> char '_') <*> many (alphaNumChar <|> char '_'))
+
+data Expr = 
+    Accept Identifier
+
+parseAccept :: Parser Expr
+parseAccept = do
+    void (string "ACCEPT")
+    space
+    Accept <$> identifier
+
+
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
+import Test.Hspec
+import Test.QuickCheck
+import Text.Megaparsec (parseMaybe)
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Parser" $ do
+    it "identifier" $ do
+      parseMaybe identifier "a" `shouldBe` Just (Identifier "a")
+      parseMaybe identifier "a1" `shouldBe` Just (Identifier "a1")
+      parseMaybe identifier "a_" `shouldBe` Just (Identifier "a_")
+      parseMaybe identifier "_1" `shouldBe` Just (Identifier "_1")
+      parseMaybe identifier "1a" `shouldBe` Nothing
