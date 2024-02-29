069e3bfd8b942a9ac3c999cc631348b96f159f9b
Guilherme S. Salustiano <guissalustiano@gmail.com>
2024-02-29 23:30:38 +0100

    start to work on add

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,8 +4,18 @@
 module Lib
     ( 
     Parser,
+
     Identifier(..),
-    identifier
+    identifier,
+    Atomic(..),
+    atomic,
+
+    Expr(..),
+    accept,
+
+    add,
+    add2,
+    add3
     ) where
 
 import Data.Void
@@ -21,18 +31,50 @@ type Parser = Parsec Void String
 newtype Identifier = Identifier String
     deriving (Show, Eq)
 
--- From https://serokell.io/blog/parser-combinators-in-haskell#identifiers
-identifier :: Parser Identifier
-identifier = Identifier <$> label "identifier" ((:) <$> (letterChar <|> char '_') <*> many (alphaNumChar <|> char '_'))
+data Atomic = 
+    AIdentifier Identifier
+    | ALiteral Integer
+    deriving (Show, Eq)
 
 data Expr = 
     Accept Identifier
+    | Add [Atomic] [Identifier] -- sources, targets
+    deriving (Show, Eq)
 
-parseAccept :: Parser Expr
-parseAccept = do
-    void (string "ACCEPT")
-    space
-    Accept <$> identifier
+-- From https://serokell.io/blog/parser-combinators-in-haskell#identifiers
+identifier :: Parser Identifier
+identifier = Identifier <$> label "identifier" ((:) <$> (letterChar <|> char '_') <*> many (alphaNumChar <|> char '_' <|> char '-'))
 
+atomic :: Parser Atomic
+atomic = (AIdentifier <$> identifier) <|> (ALiteral <$> L.decimal)
+
+accept :: Parser Expr
+accept = Accept <$> (string "ACCEPT" *> space *> identifier)
+
+add :: Parser Expr
+add = try add3 <|> add2
+
+add2 :: Parser Expr
+add2 = do
+    string "ADD" *> space
+    sources <- atomic
+
+    space *> string "TO" *> space
+    target <- identifier
+
+    return $ Add [sources] [target]
+
+add3 :: Parser Expr
+add3 = do
+    string "ADD" *> space
+    sources <- atomic `sepBy1` space
+
+    space *> string "TO" *> space
+    target <- atomic
+
+    space *> string "GIVING" *> space
+    givings <- identifier `sepBy1` space
+
+    return $ Add (target:sources) givings
 
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -7,8 +7,13 @@ main :: IO ()
 main = hspec $ do
   describe "Parser" $ do
     it "identifier" $ do
-      parseMaybe identifier "a" `shouldBe` Just (Identifier "a")
-      parseMaybe identifier "a1" `shouldBe` Just (Identifier "a1")
-      parseMaybe identifier "a_" `shouldBe` Just (Identifier "a_")
-      parseMaybe identifier "_1" `shouldBe` Just (Identifier "_1")
+      parseMaybe identifier "END" `shouldBe` Just (Identifier "END")
+      parseMaybe identifier "N-1" `shouldBe` Just (Identifier "N-1")
       parseMaybe identifier "1a" `shouldBe` Nothing
+
+    it "accept" $ do
+        parseMaybe accept "ACCEPT END" `shouldBe` Just (Accept (Identifier "END"))
+
+    it "add" $ do
+        parseMaybe add "ADD 1 2 3 TO X" `shouldBe` Just (Add [ALiteral 1, ALiteral 2, ALiteral 3] [Identifier "X"])
+        --parseMaybe add "ADD N-1 TO N GIVING CUR" `shouldBe` Just (Add [AIdentifier (Identifier "N-1")] [Identifier "N"])
